d4363e0b14443a28cf1de616c13b000ace949d7d
Phạm Anh Vũ <phamanhvu20031411@gmail.com>
2025-11-02 23:15:26 +0700

    sửa ui dịch vụ

--- src/main/java/com/company/clinicportal/view/dmdichvu/DmDichVuDetailView.java
+++ src/main/java/com/company/clinicportal/view/dmdichvu/DmDichVuDetailView.java
@@ -3,14 +3,12 @@ package com.company.clinicportal.view.dmdichvu;
 import com.company.clinicportal.entity.DmDichVu;
 import com.company.clinicportal.view.main.MainView;
 import com.vaadin.flow.router.Route;
-import io.jmix.flowui.view.EditedEntityContainer;
-import io.jmix.flowui.view.StandardDetailView;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.view.*;
 
 @Route(value = "dm-dich-vus/:id", layout = MainView.class)
 @ViewController(id = "DmDichVu.detail")
 @ViewDescriptor(path = "dm-dich-vu-detail-view.xml")
 @EditedEntityContainer("dmDichVuDc")
+@DialogMode(width = "80%", height = "100%")
 public class DmDichVuDetailView extends StandardDetailView<DmDichVu> {
 }
--- src/main/java/com/company/clinicportal/view/dmdichvu/DmDichVuListView.java
+++ src/main/java/com/company/clinicportal/view/dmdichvu/DmDichVuListView.java
@@ -1,28 +1,46 @@
 package com.company.clinicportal.view.dmdichvu;
 
+import com.company.clinicportal.entity.BuoiDieuTri;
 import com.company.clinicportal.entity.DmDichVu;
+import com.company.clinicportal.view.buoidieutri.ThuThuatDetailView;
 import com.company.clinicportal.view.main.MainView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.Messages;
+import io.jmix.flowui.DialogWindows;
 import io.jmix.flowui.UiComponents;
+import io.jmix.flowui.action.list.RemoveAction;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.kit.component.button.JmixButton;
+import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
 
 
 @Route(value = "dm-dich-vus", layout = MainView.class)
 @ViewController(id = "DmDichVu.list")
 @ViewDescriptor(path = "dm-dich-vu-list-view.xml")
 @LookupComponent("dmDichVusDataGrid")
-@DialogMode(width = "64em")
+@DialogMode(width = "80%", height = "80%")
 public class DmDichVuListView extends StandardListView<DmDichVu> {
     @Autowired
     private UiComponents uiComponents;
     @Autowired
     private Messages messages;
+    @ViewComponent
+    private DataGrid<DmDichVu> dmDichVusDataGrid;
+    @Autowired
+    private DialogWindows dialogWindows;
+    @ViewComponent
+    private CollectionLoader<DmDichVu> dmDichVusDl;
+    @ViewComponent("dmDichVusDataGrid.removeAction")
+    private RemoveAction<DmDichVu> dmDichVusDataGridRemoveAction;
 
     @Supply(to = "dmDichVusDataGrid.nhomDichVu", subject = "renderer")
     private Renderer<DmDichVu> dmDichVusDataGridNhomDichVuRenderer() {
@@ -37,4 +55,41 @@ public class DmDichVuListView extends StandardListView<DmDichVu> {
         });
     }
 
+    @Subscribe
+    public void onInit(InitEvent event) {
+        dmDichVusDataGrid.addComponentColumn(dmDichVu -> {
+                    // Tạo layout chứa hai nút
+                    HorizontalLayout actionsLayout = uiComponents.create(HorizontalLayout.class);
+
+                    // Nút Sửa
+                    JmixButton editButton = uiComponents.create(JmixButton.class);
+                    editButton.setText("Cập nhật");
+                    editButton.addClickListener(e -> {
+                        DialogWindow<DmDichVuDetailView> window = dialogWindows.detail(this, DmDichVu.class)
+                                .editEntity(dmDichVu) // chỉnh sửa entity hiện tại
+                                .withViewClass(DmDichVuDetailView.class)
+                                .build();
+                        window.addAfterCloseListener(e1 -> {
+                            dmDichVusDl.load();
+                        });
+                        window.open();
+                    });
+
+                    // Nút Xóa
+                    JmixButton deleteButton = uiComponents.create(JmixButton.class);
+                    deleteButton.setText("Xóa");
+                    deleteButton.addClickListener(e -> {
+                        dmDichVusDataGrid.select(dmDichVu);
+                        dmDichVusDataGridRemoveAction.execute();
+                    });
+
+                    // Thêm 2 nút vào layout
+                    actionsLayout.add(editButton);
+                    actionsLayout.add(deleteButton);
+
+                    return actionsLayout;
+                })
+                .setHeader("Thao tác")
+                .setAutoWidth(true);
+    }
 }
