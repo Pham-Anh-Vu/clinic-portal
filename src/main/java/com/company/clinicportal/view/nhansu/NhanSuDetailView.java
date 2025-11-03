e0dd0ddcd098b7ee506123014cf40db57f664b87
Phạm Anh Vũ <phamanhvu20031411@gmail.com>
2025-11-03 23:49:45 +0700

    sửa màn nhân sự

--- src/main/java/com/company/clinicportal/view/nhansu/NhanSuDetailView.java
+++ src/main/java/com/company/clinicportal/view/nhansu/NhanSuDetailView.java
@@ -8,8 +8,10 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.EntityStates;
+import io.jmix.core.Messages;
 import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
@@ -33,6 +35,8 @@ public class NhanSuDetailView extends StandardDetailView<NhanSu> {
     private VerticalLayout kpiMonth;
     @ViewComponent
     private VerticalLayout kpiDetail;
+    @Autowired
+    private Messages messages;
 
     @Subscribe
     public void onBeforeShow(final BeforeShowEvent event) {
@@ -46,5 +50,18 @@ public class NhanSuDetailView extends StandardDetailView<NhanSu> {
 
         buoiDieuTrisDl.setParameter("idNhanSuStaging", getEditedEntity());
         buoiDieuTrisDl.load();
+    }
+
+    @Supply(to = "buoiDieuTrisDataGrid.trangThai", subject = "renderer")
+    private Renderer<BuoiDieuTri> buoiDieuTrisDataGridTrangThaiRenderer() {
+        return new ComponentRenderer<>(buoidieutri -> {
+            // TODO: create suitable component
+            Span span = uiComponents.create(Span.class);
+            if (buoidieutri.getTrangThai()!=null) {
+                span.setText(messages.getMessage(buoidieutri.getTrangThai()));
+                span.addClassName(buoidieutri.getTrangThai().getId());
+            }
+            return span;
+        });
     }
 }
